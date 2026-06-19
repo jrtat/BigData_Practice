0028a26d48ef5e7f791026d56a11d3411d831d29
jrtat <58645976+jrtat@users.noreply.github.com>
2026-06-19 17:04:39 +0800

    改注释

--- src/main/java/org/example/SparkMySQL.java
+++ src/main/java/org/example/SparkMySQL.java
@@ -11,19 +11,20 @@ public class SparkMySQL {
                 .appName("HDFS-SparkSQL-MySQL")
                 .config("spark.sql.shuffle.partitions", "3")
                 .getOrCreate();
+        // 有了SparkSession，SparkSQL就能用了。
+        // 这里没有写 .master("local[*]")，说明运行模式完全由 spark-submit --master 决定，代码只负责逻辑，不绑定环境，更灵活。
         // appName 设置应用名称，会显示在 Spark Web UI 上
-        // config("spark.sql.shuffle.partitions", "3") 设置 shuffle 时的默认分区数
-        // 没有硬编码 master 地址，这意味着运行模式完全由 spark-submit 时的 --master 参数决定，更灵活
+        // config("spark.sql.shuffle.partitions", "3") 是 SparkSQL 内部的一个调优配置：当执行 GROUP BY、JOIN 等会引发 Shuffle 的操作时，Shuffle 后的分区数默认就是 3。
 
         Dataset<Row> df = spark.read()
                 .option("header", "true")
                 .option("inferSchema", "true")
                 .csv(SqlConfig.HDFS_PATH);
-        // 从 HDFS 读取 CSV 文件，存入一个 Dataset<Row> 中
-        // hdfsPath 指向 HDFS 上的一个 CSV 文件。
+        // SparkSQL 里“数据输入”的体现，从HDFS读取CSV文件，存入一个Dataset<Row>中
         // option("header", "true")：指定 CSV 第一行为列名
-        // option("inferSchema", "true")：让 Spark 自动推断每列的数据类型（如整数、浮点数等），否则全部默认为字符串。
-        // 随后将数据加载到一个 Dataset<Row>（即 DataFrame）中。
+        // option("inferSchema", "true")：让 Spark 自动推断每列的数据类型（如整数、浮点数等），否则全部默认为字符串
+        // 随后将数据加载到一个 Dataset<Row>（即 DataFrame）中
+        // HDFS_PATH指向HDFS上的一个 CSV 文件。
 
         System.out.println("===== 从 HDFS 读取的原始数据 =====");
         df.show();
@@ -31,7 +32,7 @@ public class SparkMySQL {
         // 在控制台打印前 20 行数据和表结构。
 
         df.createOrReplaceTempView(SqlConfig.VIEW_NAME);
-        // 将 DataFrame 注册为一个临时视图 sleep_table，之后就可以用标准 SQL 语句来查询它
+        // 将 DataFrame (df) 注册为一个临时视图 sleep_table，之后就可以用标准 SQL 语句来查询它
 
         for (String tableName : SqlConfig.TABLE_LIST) {
             String sql = SqlConfig.SQL_MAP.get(tableName); // 取出当前表名所对的SQL语句
